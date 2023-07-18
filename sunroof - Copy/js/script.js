2ebf606c3ab908159afb064576cc0ab1e16e16f7
shivam sharma <110821737+Shivam38391@users.noreply.github.com>
2023-07-18 16:44:31 +0530

    owl is working now

--- sunroof - Copy/js/script.js
+++ sunroof - Copy/js/script.js
@@ -1,19 +1,20 @@
 
 
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+$(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+   
+        navigation : true, // Show next and prev buttons
+   
+        slideSpeed : 300,
+        paginationSpeed : 400,
+   
+        items : 1, 
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false
+   
+    });
+   
+  });
