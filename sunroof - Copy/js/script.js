831a2e04e615142370bf57ccaeaf81ce1b740a25
shivam sharma <110821737+Shivam38391@users.noreply.github.com>
2023-07-19 12:48:38 +0530

    owl carousal added to brands

--- sunroof - Copy/js/script.js
+++ sunroof - Copy/js/script.js
@@ -13,8 +13,54 @@ $(document).ready(function() {
         itemsDesktop : false,
         itemsDesktopSmall : false,
         itemsTablet: false,
-        itemsMobile : false
+        itemsMobile : false,
+
+
+
+
+        animateOut: 'fadeOut',
+
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:1500,
+        autoplayHoverPause:true
    
     });
+
+
+
+
+
+    $("#owl-demo2").owlCarousel({
+
+        stagePadding: 40,
+        loop:true,
+        margin:10,
+        nav: false,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:4
+            },
+            1200:{
+                items:8
+            }
+        },
+
+
+
+
+
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:1500,
+        autoplayHoverPause:true
+
+
+      });
+     
    
   });
+
